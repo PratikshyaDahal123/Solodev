c0cff48784a11d8aae721a205aeac5e37c8c82b2
Pratikshya Dahal <pratikshyadahal92@gmail.com>
2026-04-28 10:28:00 +0545

    Add Backend/Migrations/20260519094924_AddCategories.cs

---
+++ Backend/Migrations/20260519094924_AddCategories.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Parts",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parts_CategoryId",
+                table: "Parts",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parts_Categories_CategoryId",
+                table: "Parts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parts_Categories_CategoryId",
+                table: "Parts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Parts_CategoryId",
+                table: "Parts");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Parts");
+        }
+    }
+}
