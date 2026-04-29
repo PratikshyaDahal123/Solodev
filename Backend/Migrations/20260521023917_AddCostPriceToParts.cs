6f1656bb028a8547d6720ce149d27b69ab230b76
Pratikshya Dahal <pratikshyadahal92@gmail.com>
2026-04-29 10:15:00 +0545

    Add Backend/Migrations/20260521023917_AddCostPriceToParts.cs

---
+++ Backend/Migrations/20260521023917_AddCostPriceToParts.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCostPriceToParts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CostPrice",
+                table: "Parts",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.Sql("UPDATE \"Parts\" SET \"CostPrice\" = \"UnitPrice\";");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CostPrice",
+                table: "Parts");
+        }
+    }
+}
