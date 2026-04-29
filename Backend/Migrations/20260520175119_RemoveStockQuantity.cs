21dbf3500f7a89b445d3b241e0812902010d51b6
Pratikshya Dahal <pratikshyadahal92@gmail.com>
2026-04-29 10:55:00 +0545

    Add Backend/Migrations/20260520175119_RemoveStockQuantity.cs

---
+++ Backend/Migrations/20260520175119_RemoveStockQuantity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveStockQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Parts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Parts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
