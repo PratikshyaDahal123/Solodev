4c296326e871f36fc6517b50b6bc2dbfd494d988
Pratikshya Dahal <pratikshyadahal92@gmail.com>
2026-04-29 10:22:00 +0545

    Add Backend/Migrations/20260521020137_ReAddStockQuantity.cs

---
+++ Backend/Migrations/20260521020137_ReAddStockQuantity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReAddStockQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Parts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Parts");
+        }
+    }
+}
