ba0b6b8dd5100109edd0f0edbd1dfae57dfd6ecd
Pratikshya Dahal <pratikshyadahal92@gmail.com>
2026-04-29 10:22:00 +0545

    Add Backend/Migrations/20260521020224_BackfillStockQuantityFromInvoices.cs

---
+++ Backend/Migrations/20260521020224_BackfillStockQuantityFromInvoices.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackfillStockQuantityFromInvoices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""Parts"" p
+                SET ""StockQuantity"" = GREATEST(
+                    COALESCE((SELECT SUM(pi.""Quantity"") FROM ""PurchaseInvoiceItems"" pi WHERE pi.""PartId"" = p.""PartId""), 0)
+                    - COALESCE((SELECT SUM(si.""Quantity"") FROM ""SalesInvoiceItems"" si WHERE si.""PartId"" = p.""PartId""), 0),
+                    0
+                );
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"Parts\" SET \"StockQuantity\" = 0;");
+        }
+    }
+}
