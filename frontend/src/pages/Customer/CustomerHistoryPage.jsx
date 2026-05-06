2d6270aa949dc3545115e126161966ee7e9a9350
Pratikshya Dahal <pratikshyadahal92@gmail.com>
2026-05-06 10:13:00 +0545

    Add frontend/src/pages/Customer/CustomerHistoryPage.jsx

---
+++ frontend/src/pages/Customer/CustomerHistoryPage.jsx
@@ -0,0 +1,11 @@
+import CustomerHistory from './CustomerHistory'
+
+const CustomerHistoryPage = () => {
+  return (
+    <div className="space-y-6">
+      <CustomerHistory />
+    </div>
+  )
+}
+
+export default CustomerHistoryPage
